1f2811c4b129b9b7a37bcb6f0f02d91e7945d80a
UchihaSomil <somil_j@ar.iitr.ac.in>
2021-11-13 21:54:31 +0530

    try

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 window.addEventListener('load',function(){
-    var loader_arr = ['Redirecting', 'Redirecting1', 'Redirecting2', 'Redirecting3']
+    var loader_arr = ['Somil means soft natured or a friend(nakama)', 'Leonardo Da Vinci was ambidextrous, I liked it, I learned it.', 'Both architects and UX designers orchestrate experiences']
     var loader_no = Math.floor(Math.random() * loader_arr.length);
     document.getElementById('loader-h1').textContent = loader_arr[loader_no]
     setTimeout(()=>{
