b8bec54d8d699cee0d00d51b5c142cbd5e4fd6fd
UchihaSomil <somil_j@ar.iitr.ac.in>
2022-02-15 18:11:30 +0530

    loader fixed

--- loader.js
+++ loader.js
@@ -1,7 +1,8 @@
-window.addEventListener('load',function(){
+
+window.addEventListener('DOMContentLoaded',function(){
     var loader_arr = ['Somil means soft natured or a friend(nakama)', 'Leonardo Da Vinci was ambidextrous, I liked it, I learned it.', 'Both architects and UX designers orchestrate experiences']
-    var loader_no = Math.floor(Math.random() * loader_arr.length);
-    document.getElementById('loader-h1').textContent = loader_arr[loader_no]
+var loader_no = Math.floor(Math.random() * loader_arr.length);
+document.getElementById('loader-h1').textContent = loader_arr[loader_no]
     setTimeout(()=>{
         document.getElementsByTagName('body')[0].classList.remove('overflow-hidden')
         document.getElementById('loader').style.display = "none"
@@ -9,6 +10,7 @@ window.addEventListener('load',function(){
         ;
     },2000)
 })
+
 setTimeout(()=>{
     document.getElementsByTagName('body')[0].classList.remove('overflow-hidden')
     document.getElementById('loader').style.display = "none"
